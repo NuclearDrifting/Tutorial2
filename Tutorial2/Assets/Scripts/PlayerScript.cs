a3cddb3701fe7aa1d5ab320ee7987262dcf468ef
unknown <nucleardrifting@gmail.com>
2021-10-19 19:24:23 -0400

    Tier 1

--- Tutorial2/Assets/Scripts/PlayerScript.cs
+++ Tutorial2/Assets/Scripts/PlayerScript.cs
@@ -6,18 +6,23 @@ using UnityEngine.UI;
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
-
     public float speed;
+    
 
-    public Text score;
+    public Text score; 
+    public Text winText;
 
+    private int countobj = 0;
     private int scoreValue = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        score.text = "Score: " + scoreValue.ToString();
+        countobj = 0;
+
+        winText.text = "";
     }
 
     void Update()
@@ -31,14 +36,21 @@ public class PlayerScript : MonoBehaviour
             Application.Quit();
         }
     }
-
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            countobj = countobj += 1;
+            score.text = "Score: " + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+        }
+        if (countobj == 4)
+        {
+            winText.text = "You Win! Game Created by Junior Rojas";
+            rd2d.isKinematic = true;
+            Destroy(rd2d);
         }
     }
 
@@ -48,10 +60,8 @@ public class PlayerScript : MonoBehaviour
         {
             if(Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                rd2d.AddForce(new Vector2(0, 4), ForceMode2D.Impulse);
             }
         }
-    }
-
-    
+    }   
 }
